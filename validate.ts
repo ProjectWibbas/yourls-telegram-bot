cf292d43080d0c83276b68c2d8b23f717f82f685
deriando <10672809+deriando@users.noreply.github.com>
2022-07-08 23:00:05 +0800

    .lower custom keyword

--- validate.ts
+++ validate.ts
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
 export const TeleUserMessage = Joi.object({
-  customKeyword: Joi.string().optional(),
+  customKeyword: Joi.string().optional().lowercase(),
   url: Joi.string().uri().required(),
 });
